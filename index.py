43d7b8259d08a3db93d13f1428b296579f004015
Lindsey Winters <lwinters@olivetcollege.edu>
2013-11-12 00:50:08 -0500

    assignment one finished.

--- index.py
+++ index.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return redirect(url_for('static', filename='index.html'))
+
+
+@app.route('/site<num>')
+def site(num):
+    # Works for /site01, /site02, /site03, etc.
+    return render_template('site{0}.html'.format(num))
 
 # Include a module runner to allow local testing
 if __name__ == '__main__':
